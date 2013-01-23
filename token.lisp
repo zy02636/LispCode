aa4718611f0ee0af837dc0c8b30e3b897ce1790e
zy02636 <skshoes@yahoo.cn>
2013-01-24 00:49:00 +0800

    2013-01-24 remove element from tree

--- token.lisp
+++ token.lisp
@@ -98,3 +98,40 @@
 	   (or (bst-max (node-r bst)) bst)))
 
 
+;;remove element from binary tree
+(defun bst-remove (obj bst <)
+  (if (null bst)
+      nil
+      (let ((elt (node-elt bst)))
+	(if (eql obj elt)
+	    (percolate bst)
+	  (if (funcall < obj elt)
+	      (make-node
+	       :elt elt
+	       :l (bst-remove obj (node-l bst) <)
+	       :r (node-r bst))
+	    (make-node
+	     :elt elt
+	     :r (bst-remove obj (node-r bst) <)
+	     :l (node-l bst)))))))
+
+(defun percolate (bst)
+  (cond ((null (node-l bst))
+	 (if (null (node-r bst))
+	     nil
+	   (rperc bst)))
+	((null (node-r bst)) (lperc bst))
+	(t (if (zerop (random z))
+	       (lperc bst)
+	     (rperc bst)))))
+(defun rperc (bst)
+  (make-node :elt (node-elt (node-r bst))
+	     :l (node-l bst)
+	     :r (percolate (node-r bst))))
+(defun lperc (bst)
+  (make-node :elt (node-elt (node-l bst))
+	     :l (percolate (node-l bst))
+	     :r (node-r bst)))
+
+
+
