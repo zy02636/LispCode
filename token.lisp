aab08d1a6c8e9e5662047e78b858c29ba71c840a
zy02636 <skshoes@yahoo.cn>
2013-01-23 22:17:24 +0800

    2013-01-22 binary search

--- token.lisp
+++ token.lisp
@@ -44,7 +44,57 @@
     nil))
 
 ;;define structure
-(defstruct point
-   x
-   y)
+(defstruct (point (:conc-name p)
+				  (:print-function print-point))
+  (x 0)
+  (y 0))
+
+(defun print-point (p stream depth)
+  (format stream "#<~A,~A>" (px p) (py p)))
+
+;;binary search
+;;define node structure
+(defstruct (node (:print-function
+				  (lambda (n s d)
+					(format s "#<~A>" (node-elt n)))))
+  elt (l nil) (r nil))
+
+;;define node insert function: insert value, binary search tree, operation
+(defun bst-insert (obj bst <)
+  (if (null bst)
+	  (make-node :elt obj)
+	(let ((elt (node-elt bst)))
+	  (if (eql obj elt)
+		  bst
+		(if (funcall < obj elt)
+			(make-node
+			 :elt elt
+			 :l (bst-insert obj (node-l bst) <)
+			 :r (node-r bst))
+		  (make-node
+		   :elt elt
+		   :r (bst-insert obj (node-r bst) <)
+		   :l (node-l bst)))))))
+
+;;best find
+(defun bst-find (obj bst <)
+  (if (null bst)
+	  nil
+	(let ((elt (node-elt bst)))
+	  (if (eql obj elt)
+		  bst
+		(if (funcall < obj elt)
+			(bst-find obj (node-l bst) <)
+		    (bst-find obj (node-r bst) <))))))
+
+;;binary tree min
+(defun bst-min (bst)
+  (and bst
+	   (or (bst-min (node-l bst)) bst)))
+
+;;binary tree max number 
+(defun bst-max (bst)
+  (and bst
+	   (or (bst-max (node-r bst)) bst)))
+
 
